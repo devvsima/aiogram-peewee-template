336114b560c493fb4714dedb627ca4f949cd0433
Sima <dev.simaa@gmail.com>
2023-10-12 22:19:25 +0000

    .env import list

--- data/config.py
+++ data/config.py
@@ -9,9 +9,9 @@ env.read_env()
 DIR = Path(__file__).absolute().parent.parent
 
 token_api = env.str("TOKEN", default=None)
-banned_users = env.str("BANED", default=None)
-admins = env.str("ADMINS", default=None)
-mongodb_url = env.str("MONGODB_URL", default=None)
+banned_users = [env("BANED", default=None)]
+admins = [env("ADMINS", default=None)]
+mongodb_url = [env("MONGODB_URL", default=None)]
 
 I18N_DOMAIN = 'bot'
 LOCALES_DIR = f'{DIR}\config\locales'
