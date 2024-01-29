6a53dfd76273e2bc8376fddace3489e46fef9b8c
Sima3443 <toppochka@gmail.com>
2024-01-29 12:30:30 +0200

    edit config and add libs

--- data/config.py
+++ data/config.py
@@ -11,7 +11,19 @@ DIR = Path(__file__).absolute().parent.parent
 token_api = env.str("TOKEN", default=None)
 banned_users = [env("BANED", default=None)]
 admins = [env("ADMINS", default=None)]
-mongodb_url = [env("MONGODB_URL", default=None)]
+
+
+mongo_host = env.str("MONGO_HOST", default=None)
+mongo_port = env.int("MONGH_PORT", default=27017)
+mongo_user = env.str("MONGO_USER", default=None)
+mongo_password = env.str("MONGO_PASS", default=None)
+auth_source = env.str("AUTH_SOURC", default=None)
+
+
+if not mongo_host:
+    mongodb_url = f'mongodb://localhost:{mongo_port}/'
+else:
+    mongodb_url = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/{auth_source}"
 
 I18N_DOMAIN = 'bot'
 LOCALES_DIR = f'{DIR}\config\locales'
